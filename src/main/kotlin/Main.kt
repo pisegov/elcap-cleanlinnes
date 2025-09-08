80b6d4250460d6e07a9ae0c5937b4f90568d4c70
pisegov-m <pisegov-m@yandex-team.ru>
2025-09-08 19:54:36 +0300

    move back to long polling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,8 @@ import data.local.user.UsersTable
 import dev.inmo.tgbotapi.bot.ktor.telegramBot
 import dev.inmo.tgbotapi.extensions.api.bot.getMe
 import dev.inmo.tgbotapi.extensions.api.bot.setMyCommands
-import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithFSM
-import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.startListenWebhooks
+import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithFSMAndStartLongPolling
 import dev.inmo.tgbotapi.types.BotCommand
-import io.ktor.server.netty.*
 import ioc.DaggerApplicationComponent
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -17,9 +15,6 @@ import org.jetbrains.exposed.sql.transactions.transaction
 private object EnvVars {
     const val CLEANCAP_BOT_TOKEN = "CLEANCAP_BOT_TOKEN"
     const val CLEANCAP_DATABASE_RELATIVE_PATH = "CLEANCAP_DATABASE_RELATIVE_PATH"
-    const val CLEANCAP_LISTEN_HOST = "CLEANCAP_LISTEN_HOST"
-    const val CLEANCAP_LISTEN_ROUTE = "CLEANCAP_LISTEN_ROUTE"
-    const val CLEANCAP_LISTEN_PORT = "CLEANCAP_LISTEN_PORT"
 }
 
 suspend fun main() {
@@ -36,11 +31,7 @@ suspend fun main() {
         SchemaUtils.create(AdminsTable)
     }
 
-    val listenHost = System.getenv(EnvVars.CLEANCAP_LISTEN_HOST) ?: "0.0.0.0"
-    val listenRoute = System.getenv(EnvVars.CLEANCAP_LISTEN_ROUTE) ?: "/"
-    val listenPort = System.getenv(EnvVars.CLEANCAP_LISTEN_PORT)?.toIntOrNull() ?: 8080
-
-    bot.buildBehaviourWithFSM {
+    bot.buildBehaviourWithFSMAndStartLongPolling {
         println(getMe())
 
         val applicationComponent = DaggerApplicationComponent.factory().create(this)
@@ -53,17 +44,5 @@ suspend fun main() {
             BotCommand("help", "Show help message"),
             BotCommand("call", "Обратиться к сотруднику"),
         )
-    }.run {
-
-        start()
-
-        startListenWebhooks(
-            listenPort = listenPort,
-            engineFactory = Netty,
-            exceptionsHandler = { it.printStackTrace() },
-            listenHost = listenHost,
-            listenRoute = listenRoute,
-            block = this.asUpdateReceiver
-        )
-    }
+    }.join()
 }
